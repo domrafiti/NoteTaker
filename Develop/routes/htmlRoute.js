f6d102494349dd9247dad5369ca285e0192f23a3
Dom Rafiti <dom.rafiti@gmail.com>
2021-03-21 18:46:41 -0600

    adding

--- Develop/routes/htmlRoute.js
+++ Develop/routes/htmlRoute.js
@@ -4,7 +4,7 @@ const path = require('path');
 //allows the get statements to be use by other files
 module.exports = (app) => {
     //HTML routing - GET STATEMENTS
-    app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '../public/notes.html')));
+    app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, './Develop/public/notes.html')));
     //asterix is wildcrd that catches all  - goes last
     app.get('/', (req, res) => res.sendFile(path.join(__dirname, '../public/index.html')));
 }
